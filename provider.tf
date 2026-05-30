903aebc5a6142b1f14376081fbc4392df9481c48
mandalumesh <um3514653@gmail.com>
2026-05-30 07:50:30 +0530

    code

---
+++ provider.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  
+  features {}
+  subscription_id = "386bf8df-3557-497d-bc9b-e22e349d7257"
+}
--- rg.tf
+++ rg.tf
@@ -1,9 +1,14 @@
-resource"azurerm_resource_group""rg{
+resource"azurerm_resource_group""rg" {
 name = "test-rg"
 location = "central india"
 }
 
-resource"azurerm_resource_group""rg{
+resource"azurerm_resource_group""rg1" {
 name = "test-rg2"
+location = "central india"
+}
+
+resource"azurerm_resource_group""rg2" {
+name = "test-rg3"
 location = "central india"
 }
